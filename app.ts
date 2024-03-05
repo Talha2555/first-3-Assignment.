56282a04164c4f06001169fc07876d8cb7a11e7d
Talha2555 <talha.31.khan2005@gmail.com>
2024-03-05 20:45:44 +0500

    first commit

---
+++ app.ts
@@ -0,0 +1,29 @@
+let personName: string = "ERIC";
+console.log(`Hello ${personName} would you like to learn some phython today`)
+
+
+// PRACTICE OF ASSIGNMENT 45 QUESTION
+
+// let personName:string = "TALHA"
+// console.log(`Hello ${personName} ,would you like to learn some some Typescript today?`)
+
+
+// let personName :string= "talha;";
+// console.log(`${personName.toLowerCase}`);
+// console.log(personName.toUpperCase)
+// console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase)
+
+// let  author:string = "Albert Einstein";
+// let qoute :string ="A person who never made a mistake never tried anything new."
+// console.log(`${author} once said, ${qoute}`)
+
+var str = "1"
+var str2 :number = <number> <any> str 
+console.log(typeof(str2))
+
+
+
+
+
+
+
